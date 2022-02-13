68db83d0916f402a17321bb76e232b8f6da93b6a
Blackout03 <gardner.hayden03@gmail.com>
2022-02-14 01:01:11 +1100

    Places Items back into Inventory when try to use the backpack a second time now.

--- src/main/java/com/blackout/blackoutsbackpacks/items/BackpackItem.java
+++ src/main/java/com/blackout/blackoutsbackpacks/items/BackpackItem.java
@@ -4,21 +4,15 @@ import io.netty.buffer.Unpooled;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.container.ChestContainer;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
-import net.minecraft.inventory.container.SimpleNamedContainerProvider;
 import net.minecraft.item.IDyeableArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUseContext;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
@@ -31,9 +25,7 @@ import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fml.network.NetworkHooks;
 import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.ItemHandlerHelper;
-import net.minecraftforge.items.wrapper.InvWrapper;
+import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -46,76 +38,87 @@ public class BackpackItem extends Item implements IDyeableArmorItem {
         super(properties);
     }
 
-    public static Inventory getInventory(ItemStack stack) {
-        return new Inventory(stack) {
-            @Override
-            public boolean canAddItem(@Nonnull ItemStack stack) {
-                return canAddItem(stack);
-            }
-        };
-    }
-
-    @Nonnull
     @Override
-    public ICapabilityProvider initCapabilities(ItemStack stack, CompoundNBT oldCapNbt) {
-        return new InvProvider(stack);
+    public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundNBT compound) {
+        return new InventoryCapability();
     }
-
-    private static class InvProvider implements ICapabilityProvider {
-        private final LazyOptional<IItemHandler> opt;
-
-        private InvProvider(ItemStack stack) {
-            opt = LazyOptional.of(() -> new InvWrapper(getInventory(stack)));
-        }
-
-        @Nonnull
-        @Override
-        public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction facing) {
-            return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.orEmpty(capability, opt);
-        }
-    }
-
 
     @Override
-    public ActionResult<ItemStack> use(World world, PlayerEntity player, @Nonnull Hand hand) {
-        if (!world.isClientSide) {
-            INamedContainerProvider container = new SimpleNamedContainerProvider((w, p, pl) -> ChestContainer.oneRow(player.containerMenu.containerId, player.inventory), CONTAINER_TITLE);
-            NetworkHooks.openGui((ServerPlayerEntity) player, container, buf -> {
-                buf.writeBoolean(hand == Hand.MAIN_HAND);
+    public CompoundNBT getShareTag(ItemStack stack) {
+        CompoundNBT nbt = super.getShareTag(stack);
+        if (nbt != null)
+            stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)
+                    .ifPresent(capability -> nbt.put("BackpackInventory", ((ItemStackHandler) capability).serializeNBT()));
+        return nbt;
+    }
+
+    @Override
+    public void readShareTag(ItemStack stack, @Nullable CompoundNBT nbt) {
+        super.readShareTag(stack, nbt);
+        if (nbt != null)
+            stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)
+                    .ifPresent(capability -> ((ItemStackHandler) capability).deserializeNBT((CompoundNBT) nbt.get("BackpackInventory")));
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World world, PlayerEntity entity, Hand hand) {
+        ActionResult<ItemStack> actionResult = super.use(world, entity, hand);
+        double x = entity.getX();
+        double y = entity.getY();
+        double z = entity.getZ();
+        if (entity instanceof ServerPlayerEntity) {
+            NetworkHooks.openGui((ServerPlayerEntity) entity, new INamedContainerProvider() {
+                @Override
+                public ITextComponent getDisplayName() {
+                    return CONTAINER_TITLE;
+                }
+
+                @Override
+                public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+                    PacketBuffer packetBuffer = new PacketBuffer(Unpooled.buffer());
+                    packetBuffer.writeBlockPos(new BlockPos(x, y, z));
+                    packetBuffer.writeByte(hand == Hand.MAIN_HAND ? 0 : 1);
+                    return ChestContainer.oneRow(player.containerMenu.containerId, inventory);
+                }
+            }, buf -> {
+                buf.writeBlockPos(new BlockPos(x, y, z));
+                buf.writeByte(hand == Hand.MAIN_HAND ? 0 : 1);
             });
         }
-        return ActionResult.success(player.getItemInHand(hand));
+        return actionResult;
     }
 
-    @Override
-    public ActionResultType useOn(ItemUseContext ctx) {
-        World world = ctx.getLevel();
-        BlockPos pos = ctx.getClickedPos();
-        Direction side = ctx.getClickedFace();
 
-        TileEntity tile = world.getBlockEntity(pos);
-        if (tile != null) {
-            if (!world.isClientSide) {
-                IItemHandler tileInv;
-                if (tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side).isPresent()) {
-                    tileInv = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side).orElseThrow(NullPointerException::new);
-                } else if (tile instanceof IInventory) {
-                    tileInv = new InvWrapper((IInventory) tile);
-                } else {
-                    return ActionResultType.FAIL;
-                }
+    private static class InventoryCapability implements ICapabilitySerializable<CompoundNBT> {
+        private final LazyOptional<ItemStackHandler> inventory = LazyOptional.of(this::createItemHandler);
 
-                IInventory bagInv = getInventory(ctx.getItemInHand());
-                for (int i = 0; i < bagInv.getContainerSize(); i++) {
-                    ItemStack flower = bagInv.getItem(i);
-                    ItemStack rem = ItemHandlerHelper.insertItemStacked(tileInv, flower, false);
-                    bagInv.setItem(i, rem);
-                }
-
-            }
-
-            return ActionResultType.SUCCESS;
+        @Override
+        public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction side) {
+            return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY ? this.inventory.cast() : LazyOptional.empty();
         }
-        return ActionResultType.PASS;
+
+        @Override
+        public CompoundNBT serializeNBT() {
+            return getItemHandler().serializeNBT();
+        }
+
+        @Override
+        public void deserializeNBT(CompoundNBT nbt) {
+            getItemHandler().deserializeNBT(nbt);
+        }
+
+        private ItemStackHandler createItemHandler() {
+            return new ItemStackHandler(9) {
+                @Override
+                public int getSlotLimit(int slot) {
+                    return 64;
+                }
+            };
+        }
+
+        private ItemStackHandler getItemHandler() {
+            return inventory.orElseThrow(RuntimeException::new);
+        }
     }
+
 }
